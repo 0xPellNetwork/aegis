a12fe4a1deeb8f9c90f5b1b4466227348e717fd3
jacksoom <lifengliu1994@gmail.com>
2025-06-02 17:12:08 +0800

    upgrade: add upgrade v1.4 handler (#6)

--- app/upgrades.go
+++ app/upgrades.go
@@ -4,22 +4,15 @@ import (
 	"context"
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 )
 
-const releaseVersion = "v1.2"
+const releaseVersion = "v1.4"
 
 func (app *PellApp) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(
 		releaseVersion,
 		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-			sdkCtx := sdk.UnwrapSDKContext(ctx)
-
-			if err := app.RestakingKeeper.MigrationStore(sdkCtx); err != nil {
-				return nil, err
-			}
-
 			return fromVM, nil
 		},
 	)
