b15930ecfc7d3a459ff69aa033400e35a3734ef0
jacksoom <lifengliu1994@gmail.com>
2025-06-10 18:07:05 +0800

    fix: add store upgrade to fix migrate new store

--- app/upgrades.go
+++ app/upgrades.go
@@ -3,8 +3,11 @@ package app
 import (
 	"context"
 
+	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+
+	xsecuritytypes "github.com/0xPellNetwork/aegis/x/xsecurity/types"
 )
 
 const releaseVersion = "v1.4"
@@ -13,7 +16,28 @@ func (app *PellApp) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(
 		releaseVersion,
 		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-			return fromVM, nil
+			// Set the consensus version for the new xsecurity module
+			fromVM[xsecuritytypes.ModuleName] = 1
+
+			return app.ModuleManager.RunMigrations(ctx, app.configurator, fromVM)
 		},
 	)
+
+	// Configure store upgrades for new modules
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+
+	if upgradeInfo.Name == releaseVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{xsecuritytypes.StoreKey},
+		}
+
+		// Use upgrade store loader for the initial loading of all stores when app starts,
+		// it checks if version == upgradeHeight and applies store upgrades before loading the stores,
+		// so that new stores start with the correct version (the current height of chain),
+		// instead the default which is the latest version that store last committed i.e 0 for new stores.
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
 }
