2a7e7e7abf83f317e501762c5fda8609be42d1a4
jacksoom <lifengliu1994@gmail.com>
2025-06-11 10:14:15 +0800

    skip capability init

--- app/upgrades.go
+++ app/upgrades.go
@@ -5,7 +5,10 @@ import (
 
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/ibc-go/modules/capability"
+	capabilitytypes "github.com/cosmos/ibc-go/modules/capability/types"
 
 	xsecuritytypes "github.com/0xPellNetwork/aegis/x/xsecurity/types"
 )
@@ -16,7 +19,15 @@ func (app *PellApp) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(
 		releaseVersion,
 		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-			// Set the consensus version for the new xsecurity module
+			sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+			// Check if capability module is already initialized
+			if app.CapabilityKeeper.IsInitialized(sdkCtx) {
+				// If already initialized, set its version to skip InitGenesis
+				fromVM[capabilitytypes.ModuleName] = capability.AppModule{}.ConsensusVersion()
+			}
+
+			// Set initial version for new modules only
 			fromVM[xsecuritytypes.ModuleName] = 1
 
 			return app.ModuleManager.RunMigrations(ctx, app.configurator, fromVM)
